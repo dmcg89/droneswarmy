fa52261a80a012be68adf5bae770d3713126304a
Ryan Smith <ryan.l.smith4@gmail.com>
2019-03-19 11:39:38 -0700

    server up

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+sss
+const hbs = require('express-handlebars');
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts',
+}));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
+});
