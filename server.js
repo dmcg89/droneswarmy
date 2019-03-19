71ed703b3b6dccaa15d74b05073b90b5ab28e34f
Ryan Smith <ryan.l.smith4@gmail.com>
2019-03-19 12:20:23 -0700

    added css public folder

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const app = express();
 
 const handlebars = require('express-handlebars');
 
+const path = require('path');
+
 // Lines used to set up the view engine handlebars
 app.engine('handlebars', handlebars({
   extname: 'handlebars',
   defaultLayout: 'main',
   layoutsDir: __dirname + '/views/layouts', // eslint-disable-line
 }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'handlebars');
 
