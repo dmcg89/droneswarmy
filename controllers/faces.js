0f401a15180aa3917d3df473c902f4cd949a3c20
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-03-21 01:18:45 -0700

    checks for similar faces now

--- controllers/faces.js
+++ controllers/faces.js
@@ -1,28 +1,102 @@
 var oxford = require('project-oxford');
+var client = new oxford.Client('28d68cd2760e47fc913b7b8619378b84');
+
+function includeASearchFace(groupname,imgpath,personname){
+   client.face.faceList.addFace(groupname,{
+    url : imgpath,
+    name : personname
+   })
+   .catch(function(e) {
+      console.log(e); // "Something went wrong!"
+   }).then(function (){
+      console.log("Face Added");
+   });
+}
+
+function createNewFaceList(ListIdName,ListName){
+  client.face.faceList.create(ListIdName,{
+    name : ListName
+  })
+  .catch(function(e) {
+    console.log(e); // "Something went wrong !"
+   }).then(function (){
+   console.log("Created Your FaceList");
+  });
+}
+
+function findKnownFaces(DetectId,searchFaceListName){
+  client.face.similar(DetectId,{
+    candidateFaceListId : searchFaceListName
+  }).catch(function(e) {
+     console.log(e); // "oh, no!"
+  }).then(function (response) {
+    console.log(response);
+   if (response[0] != null) {
+        if (response[0].confidence > 0.5) {
+          console.log("Good Match - Found you");
+          console.log(response);
+       }
+   } else {
+     console.log("Poor or No Match");
+    console.log(response);
+  }
+  });
+}
+
+function runAll(imageFileName,searchFaceListName){
+  client.face.detect({
+      path: imageFileName,
+      analyzesAge: true,
+      analyzesGender: true,
+      returnFaceId : true
+   }).then(function (response) {
+      DetectId = response[0].faceId;
+      findKnownFaces(DetectId,searchFaceListName);
+   });
+}
 
 module.exports = function (app) {
 
   app.get('/detect', (req, res) => {
 
-    let oxc = new oxford.Client('28d68cd2760e47fc913b7b8619378b84');
+    const facelist = 'myfaces'
 
-    loadFaces = function() {
-      const sam_url = 'https://cdn.filestackcontent.com/QGaBPrdFQnGKLXx9tGlX';
-      const ryan_url = 'https://cdn.filestackcontent.com/pZKWut31Qd6uli06dqJw';
-      oxc.face.faceList.create('myFaces');
-      oxc.face.faceList.addFace('myFaces', {url : sam_url, name : 'Sam'});
-      oxc.face.faceList.addFace('myFaces', {url : ryan_url, name : 'Ryan'});
-    }
+    const sam_url = 'https://cdn.filestackcontent.com/QGaBPrdFQnGKLXx9tGlX';
+    const ryan_url = 'https://cdn.filestackcontent.com/pZKWut31Qd6uli06dqJw';
 
-      oxc.face.detect({
-       path: './RyanPhoto.jpg',
+
+    //createNewFaceList(facelist,"students");
+    console.log("**********************")
+
+    //includeASearchFace(facelist,sam_url,'Sam');
+    //includeASearchFace(facelist,ryan_url,'Ryan');
+
+    runAll('/Users/samharrison/code/droneswarmy/controllers/RyanPhoto.jpg',facelist)
+
+
+
+    /*console.log(client.face.faceList.get(facelist));
+
+    client.face.detect({
+       path: '/Users/samharrison/code/droneswarmy/controllers/RyanPhoto.jpg',
        analyzesAge: true,
-       analyzesGender: true
-      }).then(function (response) {
-        console.log("this ran");
-        res.json(response);
+       analyzesGender: true,
+       returnFaceId: true
+    }).then(function (response) {
+      client.face.similar(response[0].faceId,{
+        candidateFaceListId : facelist
+      }).catch(function(e) {
+        console.log("oh no!");
+        console.log(e); // "oh, no!"
+      }).then (function (anotha_response) {
+        console.log("______detection")
+        console.log(anotha_response);
       });
-    });
+      console.log("this ran");
+      res.json(response);
+      console.log('this ran after');
+    });*/
+  });
 
     app.get('/', (req, res) => {
       res.render("index.handlebars");
